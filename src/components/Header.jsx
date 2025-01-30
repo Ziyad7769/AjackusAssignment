f339ca43139dbc535995d052b0182df1cd2dedb7
Ziyad7769 <khanziyad286@gmail.com>
2025-01-31 00:27:03 +0530

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between m-4'>
+        <img src="../src/assets/ajackus-logo.svg" alt="logo" />
+        <p>Ziyad Khan</p>
+    </div>
+  )
+}
+ 
+export default Header
